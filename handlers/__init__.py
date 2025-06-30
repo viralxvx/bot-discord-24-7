6698284af90ce1b4865a3ae37a2a0a9e26e3a4d0
viralxvx <miguelperaltafp@gmail.com>
2025-06-30 16:00:29 -0400

    Create __init__.py

---
+++ handlers/__init__.py
@@ -0,0 +1,5 @@
+# handlers/__init__.py
+from .go_viral import handle_go_viral_message
+from .report_menu import handle_reporte_message
+from .soporte import handle_soporte_message
+from .normas_generales import handle_normas_message
