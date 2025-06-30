20addafcbf1688106b239f564b9a4145de6ad3f4
viralxvx <miguelperaltafp@gmail.com>
2025-06-30 15:04:44 -0400

    Create reporte_incumplimiento.py

---
+++ handlers/reporte_incumplimiento.py
@@ -0,0 +1,18 @@
+import discord
+from config import CANAL_REPORTES
+from state_management import mensajes_recientes, save_state
+from views import ReportMenu
+
+async def handle_reporte_message(message):
+    if message.author.bot:
+        return
+        
+    if message.mentions:
+        reportado = message.mentions[0]
+        await message.channel.send(
+            f"📃 **Reportando a {reportado.mention}**",
+            view=ReportMenu(reportado, message.author)
+        )
+        await message.delete()
+    else:
+        await message.channel.send("⚠️ **Menciona un usuario** o usa `!permiso <días>`")
