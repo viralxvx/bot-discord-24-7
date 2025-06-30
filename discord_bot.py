1489619ca5c7018cc01734dc288031148f02a6df
viralxvx <miguelperaltafp@gmail.com>
2025-06-30 00:35:15 -0400

    Update discord_bot.py

--- discord_bot.py
+++ discord_bot.py
@@ -1,19 +1,10 @@
-from discord.ext import commands, tasks
+# discord_bot.py
 import discord
-from config import TOKEN, intents
+from discord.ext import commands
+from config import TOKEN, CANAL_OBJETIVO, ADMIN_ID  # otras constantes
 
 intents = discord.Intents.all()
 intents.members = True
 intents.message_content = True
+
 bot = commands.Bot(command_prefix="!", intents=intents)
-
-from utils import registrar_log
-
-@bot.event
-async def on_ready():
-    print(f"Bot conectado como {bot.user}")
-    await registrar_log(f"Bot iniciado")
-
-@bot.event
-async def on_member_remove(member):
-    await registrar_log(f"👋 Miembro salió: {member.name}", categoria="miembros")
