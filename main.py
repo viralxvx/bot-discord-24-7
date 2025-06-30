907ba95d390a085b2f01722af2bbb5a10bf23ac9
viralxvx <miguelperaltafp@gmail.com>
2025-06-29 23:56:49 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
-from .config import bot, engine, Session
-from .models import Base
-from .discord_bot import *
-from .commands import *
-from .tasks import *
-from .views import *
-from .message_handlers import *
-from .app import keep_alive
+import sys
+import os
+
+# Añadir el directorio actual al path para permitir importaciones absolutas
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+from config import bot, engine, Session
+from models import Base
+from discord_bot import *
+from commands import *
+from tasks import *
+from views import *
+from message_handlers import *
+from app import keep_alive
+from state_management import load_state
 
 # Crear tablas si no existen
 Base.metadata.create_all(engine)
