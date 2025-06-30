91c1692461a28cc0c8dad026b88b92a826bb9093
viralxvx <miguelperaltafp@gmail.com>
2025-06-30 17:32:35 -0400

    Delete handlers/__init__.py

--- handlers/__init__.py
+++
@@ -1,4 +0,0 @@
-from .go_viral import handle_go_viral_message
-from .report_menu import handle_reporte_message
-from .soporte import handle_soporte_message
-from .normas_generales import handle_normas_message
