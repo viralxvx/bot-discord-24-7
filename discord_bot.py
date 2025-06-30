db14d232c64b327aea0f2f9d7e0af71f1548191e
viralxvx <miguelperaltafp@gmail.com>
2025-06-30 14:50:23 -0400

    Update and rename commands.py to discord_bot.py

--- discord_bot.py
+++ discord_bot.py
@@ -1,4 +1,11 @@
 import discord
+from discord.ext import commands
+
+intents = discord.Intents.all()
+intents.members = True
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)import discord
 import datetime
 from discord_bot import bot
 from config import CANAL_REPORTES
