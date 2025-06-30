c20bd9b8ce6206f9aa6bec2315ba259a282377b1
viralxvx <miguelperaltafp@gmail.com>
2025-06-30 17:54:12 -0400

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,1 +1,10 @@
+# handlers/__init__.py
 
+from . import anuncios
+from . import faltas
+from . import go_viral
+from . import normas_generales
+from . import reporte_incumplimiento
+from . import soporte
+from . import x_normas
+from . import logs  
