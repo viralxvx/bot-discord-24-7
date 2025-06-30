ed952a7479909fa6191ebb86056e2f485c691fb5
viralxvx <miguelperaltafp@gmail.com>
2025-06-30 19:50:40 -0400

    Update on_ready.py

--- events/on_ready.py
+++ events/on_ready.py
@@ -1,10 +1,8 @@
 # events/on_ready.py
-
 import datetime
 from utils import registrar_log
-from discord_bot import bot  # Asegúrate que esta importación no causa ciclos
 
 async def on_ready():
-    print(f"✅ Bot conectado como {bot.user} (ID: {bot.user.id})")
+    print(f"✅ Bot conectado")
     print(f"Hora UTC: {datetime.datetime.utcnow().isoformat()}")
     await registrar_log("🤖 Bot listo y conectado", categoria="sistema")
