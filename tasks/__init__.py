9ca91393d6bf2fdbb01c2a89fbe1700a37a9943f
viralxvx <miguelperaltafp@gmail.com>
2025-06-30 21:22:22 -0400

    Delete tasks directory

--- tasks/__init__.py
+++
@@ -1,4 +0,0 @@
-from .clean_inactive import clean_inactive
-from .limpiar_expulsados import limpiar_expulsados
-from .reset_faltas import reset_faltas
-from .verificar_inactividad import verificar_inactividad
--- tasks/verificar_inactividad.py
+++
@@ -1,8 +0,0 @@
-# tasks/verificar_inactividad.py
-from discord.ext import tasks
-
-@tasks.loop(minutes=30)
-async def verificar_inactividad():
-    from bot_instance import bot  # Importar dentro para evitar import circular
-    await bot.wait_until_ready()
-    # Aquí pones la lógica para verificar inactividad
