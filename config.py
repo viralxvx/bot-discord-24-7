66b39f9f66cf768d31f5f8ce80028b1ac1f0087b
viralxvx <miguelperaltafp@gmail.com>
2025-06-30 22:12:02 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,12 @@
+# config.py
+
 import os
 
 TOKEN = os.getenv("DISCORD_TOKEN")
-REDIS_URL = os.getenv("REDIS_URL")
-CANAL_OBJETIVO = int(os.getenv("CANAL_OBJETIVO", 0))
-CANAL_FALTAS = int(os.getenv("CANAL_FALTAS", 0))
-CANAL_LOGS = int(os.getenv("CANAL_LOGS", 0))
+
+CANAL_OBJETIVO = int(os.getenv("CANAL_OBJETIVO", "0"))
+CANAL_LOGS = int(os.getenv("CANAL_LOGS", "0"))
+CANAL_FALTAS = int(os.getenv("CANAL_FALTAS", "0"))
+CANAL_REPORTE = int(os.getenv("CANAL_REPORTE", "0"))
+
+MAX_LOG_LENGTH = 1900  # Puedes ajustar este valor si quieres
