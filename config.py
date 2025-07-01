8383aa9ddd4a87cac5f991f1c0c0f963094980b7
viralxvx <miguelperaltafp@gmail.com>
2025-06-30 23:02:57 -0400

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 REDIS_URL = os.getenv("REDIS_URL")
-CANAL_OBJPID = int(os.getenv("CANAL_OBJETIVO"))
+CANAL_OBJETIVO = int(os.getenv("CANAL_OBJETIVO"))
 CANAL_FALTAS = int(os.getenv("CANAL_FALTAS"))
 CANAL_LOGS = int(os.getenv("CANAL_LOGS"))
 CANAL_REPORTE = int(os.getenv("CANAL_REPORTE"))
