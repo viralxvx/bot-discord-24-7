12c21d95bf87ed145b15b334c9ac472af7150dd7
viralxvx <miguelperaltafp@gmail.com>
2025-06-30 21:22:05 -0400

    Delete events directory

--- events/on_ready.py
+++
@@ -1,8 +0,0 @@
-# events/on_ready.py
-import datetime
-from utils import registrar_log
-
-async def on_ready():
-    print(f"✅ Bot conectado")
-    print(f"Hora UTC: {datetime.datetime.utcnow().isoformat()}")
-    await registrar_log("🤖 Bot listo y conectado", categoria="sistema")
