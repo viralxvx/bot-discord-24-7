6aa181d931e61568f6659c57e143d0753760bb4e
viralxvx <miguelperaltafp@gmail.com>
2025-07-02 17:39:54 -0400

    Create normas_texto.py

---
+++ mensajes/normas_texto.py
@@ -0,0 +1,13 @@
+TITULO = "📜 NORMAS GENERALES DE VX"
+
+IMAGEN_URL = "https://drive.google.com/uc?export=download&id=1LGwse5dI_Q_PpQhhfpLBudteATKoy4Hj"
+
+DESCRIPCION = """
+### 🎉 ¡Bienvenido a nuestra comunidad Viral 𝕏 | V𝕏! 🎉
+
+Este canal contiene las normas generales que TODOS los miembros deben conocer y respetar para garantizar el orden, el crecimiento y el buen funcionamiento de nuestra comunidad. Nuestro objetivo es que nos apoyemos conjuntamente y nos ayudemos a crecer, compartiendo contenido de VALOR para nuestras audiencias.
+
+... (PEGAS AQUÍ TODO TU MENSAJE DE NORMAS COMPLETO)
+
+*Bot actualizado • Sistema automatizado • Comunidad en crecimiento 🚀*
+"""
