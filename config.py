7a9b00629cf2e4408eb2c1cba6a13dfdf39c7d1a
viralxvx <miguelperaltafp@gmail.com>
2025-07-03 00:24:22 -0400

    Update config.py

--- config.py
+++ config.py
@@ -3,14 +3,18 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-ADMIN_ID = int(os.getenv("ADMIN_ID"))
-CANAL_FALTAS = int(os.getenv("CANAL_FALTAS"))
-CANAL_LOGS = int(os.getenv("CANAL_LOGS"))
-CANAL_OBJETIVO = int(os.getenv("CANAL_OBJETIVO"))
-CANAL_REPORTE = int(os.getenv("CANAL_REPORTE"))
-CANAL_COMANDOS = int(os.getenv("CANAL_COMANDOS"))
+# IDs de canales (se cargan desde variables de entorno)
+CANAL_PRESENTATE_ID = int(os.getenv("CANAL_PRESENTATE"))
+CANAL_NORMAS_ID = int(os.getenv("CANAL_NORMAS"))
+CANAL_FALTAS_ID = int(os.getenv("CANAL_FALTAS"))
+CANAL_LOGS_ID = int(os.getenv("CANAL_LOGS"))
+CANAL_COMANDOS_ID = int(os.getenv("CANAL_COMANDOS"))
+CANAL_OBJETIVO_ID = int(os.getenv("CANAL_OBJETIVO"))
+CANAL_REPORTE_ID = int(os.getenv("CANAL_REPORTE"))
 
-DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
+# Otros datos
+ADMIN_ID = int(os.getenv("ADMIN_ID"))
 GUILD_ID = int(os.getenv("GUILD_ID"))
 REDIS_URL = os.getenv("REDIS_URL")
-STATE_PATH = os.getenv("STATE_PATH", "./state.json")
+STATE_PATH = os.getenv("STATE_PATH")
+DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
