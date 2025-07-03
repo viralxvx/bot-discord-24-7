b5e5e46d2d0f38cb2cefae88f611734c0c2d612a
viralxvx <miguelperaltafp@gmail.com>
2025-07-03 00:47:03 -0400

    Update config.py

--- config.py
+++ config.py
@@ -4,14 +4,19 @@ def get_env_variable(var_name):
     value = os.getenv(var_name)
     if value is None:
         raise ValueError(f"❌ Error: la variable de entorno '{var_name}' no está definida.")
-    return int(value)
+    return int(value) if var_name != "REDIS_URL" else value
 
-CANAL_GUIAS_ID = get_env_variable("CANAL_GUIAS")       # 📖guías
-CANAL_COMANDOS_ID = get_env_variable("CANAL_COMANDOS") # 💻comandos
+# IDs de canales
+CANAL_GUIAS_ID = get_env_variable("CANAL_GUIAS")
+CANAL_COMANDOS_ID = get_env_variable("CANAL_COMANDOS")
 CANAL_PRESENTATE_ID = get_env_variable("CANAL_PRESENTATE")
 CANAL_NORMAS_ID = get_env_variable("CANAL_NORMAS")
 CANAL_FALTAS_ID = get_env_variable("CANAL_FALTAS")
 CANAL_LOGS_ID = get_env_variable("CANAL_LOGS")
 CANAL_OBJETIVO_ID = get_env_variable("CANAL_OBJETIVO")
 CANAL_REPORTE_ID = get_env_variable("CANAL_REPORTE")
+CANAL_VICTORIAS_ID = get_env_variable("CANAL_VICTORIAS")
+
+# Otros valores
 ADMIN_ID = get_env_variable("ADMIN_ID")
+REDIS_URL = os.getenv("REDIS_URL")  # Redirige sin convertir a int
