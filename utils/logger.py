73f0c5366bded1e6125884553b94c4646ee9f3ce
viralxvx <miguelperaltafp@gmail.com>
2025-07-05 12:11:19 -0400

    Update logger.py

--- utils/logger.py
+++ utils/logger.py
@@ -1,13 +1,12 @@
-import logging
-import sys
 import discord
+import logging
 
 # Crear un logger centralizado
 logger = logging.getLogger("discord")
 logger.setLevel(logging.INFO)  # Esto es para que los logs importantes se registren
 
 # Crear un manejador para logs de consola (Railway)
-console_handler = logging.StreamHandler(sys.stdout)
+console_handler = logging.StreamHandler()
 console_handler.setLevel(logging.INFO)
 
 # Crear un formateador para los logs
@@ -21,17 +20,19 @@ logger.addHandler(console_handler)
 
 # Función para loguear los mensajes en el canal de Discord
 async def log_discord(bot, message, level="info", title="Log"):
+    # Obtener el canal de logs desde las variables de entorno o como está configurado
     log_channel = bot.get_channel(CANAL_LOGS)  # Cambia esto con el ID del canal de logs
-    embed = discord.Embed(
-        title=title,
-        description=message,
-        color=discord.Color.green() if level == "info" else discord.Color.red()
-    )
-    embed.set_footer(text=f"Nivel: {level}")
-    await log_channel.send(embed=embed)
+    if log_channel:
+        embed = discord.Embed(
+            title=title,
+            description=message,
+            color=discord.Color.green() if level == "info" else discord.Color.red()
+        )
+        embed.set_footer(text=f"Nivel: {level}")
+        await log_channel.send(embed=embed)
 
 # Función para manejar los logs de Railway y Discord
-def custom_log(level, message, bot=None, title=""):
+def custom_log(bot, level, message, title=""):
     if level == "warning":
         logger.warning(message)
     elif level == "info":
