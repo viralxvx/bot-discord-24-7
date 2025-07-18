3ae0b1bfc86799c59b3fb64ce49459c8ad7f2daa
viralxvx <miguelperaltafp@gmail.com>
2025-07-18 11:28:36 -0400

    Update and rename mensaje_telegram.py to mensajes_telegram.py

---
+++ mensajes/mensajes_telegram.py
@@ -0,0 +1,16 @@
+# mensajes/telegram.py
+
+BIENVENIDA = """
+👋 ¡Bienvenido a VXbot!
+
+Para comenzar, escribe exactamente: *Quiero Viralizar*
+"""
+
+AYUDA = """
+¿Necesitas ayuda? Escribe *Quiero Viralizar* para iniciar el proceso.
+"""
+
+ERROR = """
+❌ Ocurrió un error. Por favor, intenta de nuevo más tarde.
+"""
+
