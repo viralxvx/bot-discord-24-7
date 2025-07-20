789505387b6d85c268c49eee968d915cf45347ed
viralxvx <miguelperaltafp@gmail.com>
2025-07-19 22:46:24 -0400

    Update config.py

--- vx-viral-assistant/config.py
+++ vx-viral-assistant/config.py
@@ -11,6 +11,5 @@ def get_env(name, required=True):
 DISCORD_TOKEN = get_env("DISCORD_TOKEN")
 OPENAI_API_KEY = get_env("OPENAI_API_KEY")
 GUILD_ID = int(get_env("GUILD_ID"))
-CANAL_COMANDOS_ID = int(get_env("CANAL_COMANDOS_ID"))
 CANAL_GPT_ID = int(get_env("CANAL_GPT_ID"))
 ADMIN_ID = int(get_env("ADMIN_ID"))
